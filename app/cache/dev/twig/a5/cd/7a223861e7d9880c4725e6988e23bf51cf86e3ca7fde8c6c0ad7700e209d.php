c927e9c2fe1f6a01c51dd59566ff44347ac1e3b5
Adam Sunderman <amsunderman@gmail.com>
2014-12-11 17:24:09 -0600

    commit

---
+++ app/cache/dev/twig/a5/cd/7a223861e7d9880c4725e6988e23bf51cf86e3ca7fde8c6c0ad7700e209d.php
@@ -0,0 +1,49 @@
+<?php
+
+/* default/index.html.twig */
+class __TwigTemplate_a5cd7a223861e7d9880c4725e6988e23bf51cf86e3ca7fde8c6c0ad7700e209d extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("base.html.twig");
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        // line 4
+        echo "    Homepage.
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "default/index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 4,  28 => 3,);
+    }
+}
